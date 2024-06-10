4861890a19c35ba327a223f27599e4411b8bcd8b
Phanikumar Kundeti (pkundeti) <pkundeti@cisco.com>
2024-06-10 21:40:32 +0530

    Updated files

--- aws-terraform/runner_cloudinit.tf
+++ aws-terraform/runner_cloudinit.tf
@@ -9,7 +9,7 @@ data "cloudinit_config" "circleci_linux_runner_user_data" {
     content = templatefile(
       "${path.module}/templates/setup-circleci-runner.sh.tpl",
       {
-        cci_runner_token = jsondecode(data.aws_secretsmanager_secret_version.cci_linux_runner_token_secret_version.secret_string)["circleci_runner_token"]
+        cci_runner_token = jsondecode(data.aws_secretsmanager_secret_version.cci_linux_runner_token_secret_version.secret_string)["circleci_runner_secret"]
       }
     )
   }
