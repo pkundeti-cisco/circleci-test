767491a4b54fd4793e50856de94f0e09248743e2
Phanikumar Kundeti (pkundeti) <pkundeti@cisco.com>
2024-06-11 20:55:52 +0530

    Update asg.tf

--- aws-terraform/asg.tf
+++ aws-terraform/asg.tf
@@ -4,8 +4,8 @@ resource "aws_autoscaling_group" "linux_runner_asg" {
   name                 = "${var.runner_prefix}_asg"
   availability_zones   = ["us-east-1a"]
   desired_capacity     = 0
-  max_size             = 0
-  min_size             = 1
+  max_size             = 1
+  min_size             = 0
   termination_policies = ["OldestInstance"]
   launch_template {
     id      = aws_launch_template.linux_runner_launch_template.id
